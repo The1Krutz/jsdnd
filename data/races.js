aaa14358ef1b708b34eb90eccc9708db1eea5cbc
Thomas <the1krutz@gmail.com>
2019-01-31 23:37:59 -0800

    basically everything?

---
+++ data/races.js
@@ -0,0 +1,644 @@
+export default [
+    {
+        "name": "Aarakocra",
+        "base": null,
+        "statmods": "Dex+2,Wis+1",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Aasimar",
+        "base": null,
+        "statmods": "Cha+2",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Abyssal Tiefling",
+        "base": "Variant Tiefling",
+        "statmods": "Con+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Aetherborn",
+        "base": null,
+        "statmods": "Cha+2,%+1,%+1",
+        "source": "Plane Shift: Kaladesh"
+    },
+    {
+        "name": "Air Genasi",
+        "base": "Genasi",
+        "statmods": "Dex+1",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Asmodeus",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Int+1,Cha+2",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Aven (Amonkhet)",
+        "base": null,
+        "statmods": "Dex+2",
+        "source": "Plane Shift: Amonkhet"
+    },
+    {
+        "name": "Aven (Dominaria)",
+        "base": null,
+        "statmods": "Dex+2,Wis+2",
+        "source": "Plane Shift: Dominaria"
+    },
+    {
+        "name": "Baalzebul",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Int+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Beasthide",
+        "base": "Shifter",
+        "statmods": "Con+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Blue Merfolk",
+        "base": "Merfolk",
+        "statmods": "Int+2",
+        "source": "Plane Shift: Ixalan"
+    },
+    {
+        "name": "Bugbear",
+        "base": null,
+        "statmods": "Str+2,Dex+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Centaur",
+        "base": null,
+        "statmods": "Str+2,Wis+1",
+        "source": "Guildmasters' Guide to Ravnica"
+    },
+    {
+        "name": "Changeling",
+        "base": null,
+        "statmods": "Dex+1,Cha+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Cliffwalk",
+        "base": "Shifter",
+        "statmods": "Dex+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Cosi Creed",
+        "base": "Merfolk",
+        "statmods": "Int+1,Cha+1",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Dark Elf",
+        "base": "Elf",
+        "statmods": "Cha+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Deep Gnome",
+        "base": "Gnome",
+        "statmods": "Dex+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Dispater",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Dex+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Dragonborn",
+        "base": null,
+        "statmods": "Str+2,Cha+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Dragonborn",
+        "base": null,
+        "statmods": "Str+2,Cha+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Dwarf",
+        "base": null,
+        "statmods": "Con+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Dwarf",
+        "base": null,
+        "statmods": "Con+2",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Earth Genasi",
+        "base": "Genasi",
+        "statmods": "Str+1",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Eladrin",
+        "base": "Elf",
+        "statmods": "Cha+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Elf",
+        "base": null,
+        "statmods": "Dex+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Elf",
+        "base": null,
+        "statmods": "Dex+2",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Emeria Creed",
+        "base": "Merfolk",
+        "statmods": "Wis+2",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Fallen Aasimar",
+        "base": "Aasimar",
+        "statmods": "Str+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Fierna",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Wis+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Firbolg",
+        "base": null,
+        "statmods": "Wis+2,Str+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Fire Genasi",
+        "base": "Genasi",
+        "statmods": "Int+1",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Forest Gnome",
+        "base": "Gnome",
+        "statmods": "Dex+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Genasi",
+        "base": null,
+        "statmods": "Con+2",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Gith",
+        "base": null,
+        "statmods": "Int+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Githyanki",
+        "base": "Gith",
+        "statmods": "Str+2",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Githzerai",
+        "base": "Gith",
+        "statmods": "Wis+2",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Glasya",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Dex+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Gnome",
+        "base": null,
+        "statmods": "Int+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Gnome",
+        "base": null,
+        "statmods": "Int+2",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Goblin",
+        "base": null,
+        "statmods": "Dex+2,Con+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Goliath",
+        "base": null,
+        "statmods": "Str+2,Con+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Green Merfolk",
+        "base": "Merfolk",
+        "statmods": "Wis+2",
+        "source": "Plane Shift: Ixalan"
+    },
+    {
+        "name": "Half-Elf",
+        "base": null,
+        "statmods": "Cha+2,%+1,%+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Half-Elf",
+        "base": null,
+        "statmods": "Cha+2,%+1,%+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Half-Orc",
+        "base": null,
+        "statmods": "Str+2,Con+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Half-Orc",
+        "base": null,
+        "statmods": "Str+2,Con+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Halfling",
+        "base": null,
+        "statmods": "Dex+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Halfling",
+        "base": null,
+        "statmods": "Dex+2",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Hawk-Headed Aven",
+        "base": "Aven (Amonkhet)",
+        "statmods": "Wis+2",
+        "source": "Plane Shift: Amonkhet"
+    },
+    {
+        "name": "High Elf",
+        "base": "Elf",
+        "statmods": "Int+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "High Elf",
+        "base": "Elf",
+        "statmods": "Int+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Hill Dwarf",
+        "base": "Dwarf",
+        "statmods": "Wis+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Hill Dwarf",
+        "base": "Dwarf",
+        "statmods": "Wis+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Hobgoblin",
+        "base": null,
+        "statmods": "Con+2,Int+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Human",
+        "base": null,
+        "statmods": "Str+1,Dex+1,Con+1,Int+1,Wis+1,Cha+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Human",
+        "base": null,
+        "statmods": "Str+1,Dex+1,Con+1,Int+1,Wis+1,Cha+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Ibis-Headed Aven",
+        "base": "Aven (Amonkhet)",
+        "statmods": "Int+1",
+        "source": "Plane Shift: Amonkhet"
+    },
+    {
+        "name": "Infernal Tiefling",
+        "base": "Variant Tiefling",
+        "statmods": "Int+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Kalashtar",
+        "base": null,
+        "statmods": "Wis+1,Cha+1,%+1",
+        "source": "Wayfinder's Guide to Eberron"
+    },
+    {
+        "name": "Kenku",
+        "base": null,
+        "statmods": "Dex+2,Wis+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Khenra",
+        "base": null,
+        "statmods": "Dex+2,Str+1",
+        "source": "Plane Shift: Amonkhet"
+    },
+    {
+        "name": "Kobold",
+        "base": null,
+        "statmods": "Dex+2,Str-2",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Kor",
+        "base": null,
+        "statmods": "Dex+2,Wis+1",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Levistus",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Con+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Lightfoot",
+        "base": "Halfling",
+        "statmods": "Cha+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Lightfoot",
+        "base": "Halfling",
+        "statmods": "Cha+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Lizardfolk",
+        "base": null,
+        "statmods": "Con+2,Wis+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Longstride",
+        "base": "Shifter",
+        "statmods": "Dex+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Longtooth",
+        "base": "Shifter",
+        "statmods": "Str+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Loxodon",
+        "base": null,
+        "statmods": "Con+2,Wis+1",
+        "source": "Guildmasters' Guide to Ravnica"
+    },
+    {
+        "name": "Mammon",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Int+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Mephistopheles",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Int+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Merfolk",
+        "base": null,
+        "statmods": "Cha+1",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Minotaur",
+        "base": null,
+        "statmods": "Str+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Mountain Dwarf",
+        "base": "Dwarf",
+        "statmods": "Str+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Naga",
+        "base": null,
+        "statmods": "Con+2,Int+1",
+        "source": "Plane Shift: Amonkhet"
+    },
+    {
+        "name": "Orc",
+        "base": null,
+        "statmods": "Str+2,Con+2,Int-2",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Protector Aasimar",
+        "base": "Aasimar",
+        "statmods": "Wis+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Razortooth",
+        "base": "Shifter",
+        "statmods": "Dex+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Rock Gnome",
+        "base": "Gnome",
+        "statmods": "Con+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Rock Gnome",
+        "base": "Gnome",
+        "statmods": "Con+1",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Scourge Aasimar",
+        "base": "Aasimar",
+        "statmods": "Con+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Sea Elf",
+        "base": "Elf",
+        "statmods": "Con+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Shadar-Kai",
+        "base": "Elf",
+        "statmods": "Con+1",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Shifter",
+        "base": null,
+        "statmods": "Dex+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Simic Hybrid",
+        "base": null,
+        "statmods": "Con+2,%+1",
+        "source": "Guildmasters' Guide to Ravnica"
+    },
+    {
+        "name": "Siren",
+        "base": null,
+        "statmods": "Cha+2",
+        "source": "Plane Shift: Ixalan"
+    },
+    {
+        "name": "Stout",
+        "base": "Halfling",
+        "statmods": "Con+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Tabaxi",
+        "base": null,
+        "statmods": "Dex+2,Cha+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Tiefling",
+        "base": null,
+        "statmods": "Int+1,Cha+2",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Tiefling",
+        "base": null,
+        "statmods": "Int+1,Cha+2",
+        "source": "System Resource Document v5.1"
+    },
+    {
+        "name": "Tiefling (MtoF)",
+        "base": null,
+        "statmods": "",
+        "source": "Mordenkainen's Tome of Foes"
+    },
+    {
+        "name": "Tortle",
+        "base": null,
+        "statmods": "Str+2,Wis+1",
+        "source": "Tomb of Annihilation"
+    },
+    {
+        "name": "Triton",
+        "base": null,
+        "statmods": "Str+1,Con+1,Cha+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Ula Creed",
+        "base": "Merfolk",
+        "statmods": "Int+2",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Vampire",
+        "base": null,
+        "statmods": "Cha+2,Wis+1",
+        "source": "Plane Shift: Ixalan"
+    },
+    {
+        "name": "Vampire",
+        "base": null,
+        "statmods": "Int+1,Cha+2",
+        "source": "Plane Shift: Zendikar"
+    },
+    {
+        "name": "Variant Tiefling",
+        "base": null,
+        "statmods": "Cha+2",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Vedalken",
+        "base": null,
+        "statmods": "Int+2,Wis+1",
+        "source": "Guildmasters' Guide to Ravnica"
+    },
+    {
+        "name": "Viashino",
+        "base": null,
+        "statmods": "Dex+2,Str+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Warforged",
+        "base": null,
+        "statmods": "Str+1,Con+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Water Genasi",
+        "base": "Genasi",
+        "statmods": "Wis+1",
+        "source": "Elemental Evil"
+    },
+    {
+        "name": "Wildhunt",
+        "base": "Shifter",
+        "statmods": "Wis+1",
+        "source": "Unearthed Arcana"
+    },
+    {
+        "name": "Wood Elf",
+        "base": "Elf",
+        "statmods": "Wis+1",
+        "source": "Player's Handbook"
+    },
+    {
+        "name": "Yuan-ti Pureblood",
+        "base": null,
+        "statmods": "Cha+2,Int+1",
+        "source": "Volo's Guide to Monsters"
+    },
+    {
+        "name": "Zariel",
+        "base": "Tiefling (MtoF)",
+        "statmods": "Cha+2,Str+1",
+        "source": "Mordenkainen's Tome of Foes"
+    }
+]
---
+++ src/index.js
@@ -0,0 +1,179 @@
+import classes from '../data/classes.js';
+import races from '../data/races.js';
+import backgrounds from '../data/backgrounds.js';
+
+const config = {
+    statgen: 'alltens',
+    permittedsources: [],
+    allsources: []
+}
+
+// variety of ways to generate 6 random stats
+const statgen = {
+    alltens: () => [10, 10, 10, 10, 10, 10],
+    standardarray: () => [15, 14, 13, 12, 10, 8],
+    _3to18: () => [
+        dieroll(18, 3),
+        dieroll(18, 3),
+        dieroll(18, 3),
+        dieroll(18, 3),
+        dieroll(18, 3),
+        dieroll(18, 3)
+    ],
+    _3d6: () => [
+        dieroll(6) + dieroll(6) + dieroll(6),
+        dieroll(6) + dieroll(6) + dieroll(6),
+        dieroll(6) + dieroll(6) + dieroll(6),
+        dieroll(6) + dieroll(6) + dieroll(6),
+        dieroll(6) + dieroll(6) + dieroll(6),
+        dieroll(6) + dieroll(6) + dieroll(6),
+    ],
+    _4d6droplowest: () => {
+        // todo
+    }
+}
+
+const na = {
+    name: 'n/a',
+    source: null
+}
+
+function dieroll(high, low) {
+    if (low == null) { low = 1 }
+    return Math.floor((Math.random() * (high - low)) + low);
+}
+
+function processstatmods(stats, modstring) {
+    if (modstring.endsWith(',')) {
+        modstring = modstring.slice(0, modstring.length - 1)
+    }
+    if (modstring.startsWith(',')) {
+        modstring = modstring.slice(1, modstring.length)
+    }
+
+    let mods = modstring.toLocaleLowerCase().split(',');
+    const allstats = ['str', 'dex', 'con', 'int', 'wis', 'cha'];
+
+    for (let mod of mods) {
+        let adding = mod.split('+');
+        if (adding.length !== 1) {
+            if (adding[0] === '%') {
+                adding[0] = allstats[dieroll(allstats.length, 0)]
+            }
+            stats[adding[0]] += +adding[1]
+            allstats.splice(allstats.indexOf(adding[0]), 1)
+
+            continue;
+        }
+
+        let subtracting = mod.split('-');
+        if (subtracting.length !== 1) {
+            if (subtracting[0] === '%') {
+                subtracting[0] = allstats[dieroll(allstats.length, 0)]
+            }
+            stats[subtracting[0]] -= +subtracting[1]
+            allstats.splice(allstats.indexOf(subtracting[0]), 1)
+
+            continue;
+        }
+        throw new Error('messed up stat mod, check your data', modstring)
+    }
+}
+
+function assemble() {
+    let character = {
+        class: null,
+        subclass: null,
+        race: null,
+        subrace: null,
+        background: null,
+        stats: {}
+    };
+    let permittedclasses = classes.filter(z => config.permittedsources.indexOf(z.source) !== -1)
+    let permittedraces = races.filter(z => config.permittedsources.indexOf(z.source) !== -1)
+    let permittedbackgrounds = backgrounds.filter(z => config.permittedsources.indexOf(z.source) !== -1)
+
+    let availableclasses = permittedclasses.filter(z => z.base == null);
+    character.class = availableclasses[dieroll(availableclasses.length, 0)];
+
+    let availablesubclasses = permittedclasses.filter(z => z.base === character.class.name);
+    character.subclass = availablesubclasses[dieroll(availablesubclasses.length, 0)] || na;
+
+    let availableraces = permittedraces.filter(z => z.base == null);
+    character.race = availableraces[dieroll(availableraces.length, 0)];
+
+    let availablesubraces = permittedraces.filter(z => z.base == character.race.name);
+    character.subrace = availablesubraces[dieroll(availablesubraces.length, 0)] || na;
+
+    character.background = permittedbackgrounds[dieroll(permittedbackgrounds.length, 0)]
+
+    let [str, dex, con, int, wis, cha] = statgen[config.statgen]();
+    character.stats.str = str;
+    character.stats.dex = dex;
+    character.stats.con = con;
+    character.stats.int = int;
+    character.stats.wis = wis;
+    character.stats.cha = cha;
+
+    processstatmods(character.stats, [character.race.statmods, character.subrace.statmods].join(','))
+
+    return character;
+}
+
+function togglepermittedsource(source) {
+    let ind = config.permittedsources.indexOf(source)
+    if (ind === -1) {
+        config.permittedsources.push(source);
+    } else {
+        config.permittedsources.splice(ind, 1)
+    }
+
+    console.log(config.permittedsources.sort())
+}
+
+for (let item of classes.concat(races).concat(backgrounds)) {
+    if (config.allsources.indexOf(item.source) === -1) {
+        config.allsources.push(item.source)
+        config.permittedsources.push(item.source)
+    }
+}
+config.allsources.sort()
+config.permittedsources.sort()
+
+let bookselectors = document.getElementById('booklist');
+for (let s of config.allsources) {
+    let eid = `${s} toggle`
+
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.id = eid;
+    checkbox.onchange = () => { togglepermittedsource(s) }
+    checkbox.checked = true;
+
+    let label = document.createElement('label');
+    label.htmlFor = eid;
+    label.className = 'booktoggle';
+    label.innerText = s;
+
+    label.prepend(checkbox);
+    bookselectors.appendChild(label);
+}
+
+document.getElementById('assembler').addEventListener('click', () => {
+    let c = assemble();
+    let sources = [];
+    for (let s of [c.race.source, c.subrace.source, c.class.source, c.subclass.source, c.background.source]) {
+        if (s && sources.indexOf(s) === -1) {
+            sources.push(s);
+        }
+    }
+    sources.sort()
+
+    let outputsection = document.getElementById('output');
+
+    outputsection.children.race.innerHTML = `Race: ${c.race.name} (${c.subrace.name})`
+    outputsection.children.class.innerHTML = `Class: ${c.class.name} (${c.subclass.name})`
+    outputsection.children.background.innerHTML = `Background: ${c.background.name}`
+    outputsection.children.sources.innerHTML = `Sources:<br />${sources.join('<br />')}`
+
+}, false)
